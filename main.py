09abb4321ffff644a992e2e3a3dbb0f4f6be0856
kenan <kenank428@gmail.com>
2023-05-01 15:19:49 +0200

    updated

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+import random
+
+#These are constants that can be changed to change the game
 MAX_LINES = 3
-MAX_BET = 5
+MAX_BET = 100
+
+ROWS = 3
+COLS = 3
+
+#This is a dictionary that maps a symbol to a number
+symbol_count = {
+    "A": 2,
+    "B": 6,
+    "C": 4,
+    "D": 8,
+}
 
 def deposit():
     while True:
         amount = input("What would you like to depsit? $")
+        # Check if the amount is a digit
         if amount.isdigit():
+            # Convert the amount to an integer
             amount = int(amount)
+            # Check if the amount is greater than 0
             if amount > 0:
                 break
             else:
@@ -16,16 +33,16 @@ def deposit():
 
 def get_number_of_lines():
     while True:
-        number_of_lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
-        if number_of_lines.isdigit():
-            number_of_lines = int(number_of_lines)
-            if number_of_lines > 0 and number_of_lines <= MAX_LINES:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")# we make a concatenation to make sure that the user can only enter a number between 1 and 3
+        if lines.isdigit():
+            lines = int(lines)
+            if lines > 0 and lines <= MAX_LINES:
                 break
             else:
                 print("Invalid number of lines, number of lines must be between 1 and 3")
         else:
             print("Please enter a valid number of lines")
-    return number_of_lines
+    return lines
 
 def get_bet():
     while True:
@@ -40,11 +57,17 @@ def get_bet():
             print("Please enter a valid bet")
     return bet
 
+#We use the main function so that if we end our program, we can start it again
 def main():
     balance = deposit()
-    number_of_lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = number_of_lines * bet
-    print(f"You are betting ${bet} on {number_of_lines} lines. Total bet is equal to ${total_bet}")
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = lines * bet
+        if total_bet > balance:
+            print("You do not have enough money to make this bet, your current balance is ${balance}")
+        else:
+            break
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}")
 
 main()
