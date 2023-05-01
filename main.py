b6d2a06dbd466bae5a8c01826f0f330fe7ca28e0
kenan <kenank428@gmail.com>
2023-05-01 15:45:39 +0200

    Updated to be continued...

--- main.py
+++ main.py
@@ -15,6 +15,40 @@ symbol_count = {
     "D": 8,
 }
 
+def get_slot_machine_spin(rows, cols, symbols):
+    #Create a list that will randomly select values for each of columns:
+    #Create a list that contains all of the different values we possibly could select from
+    #Randomly choose 3 values from the list
+    #We choose the value, we remove it and then choose again.
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():#.items gives us both the key and the value associated with the dictionary
+        for _ in range(symbol_count): #we use _ because we don't care about the value of the variable and it avoids having to create a variable
+            all_symbols.append(symbol) #append is a function that adds a value to the end of a list
+
+    #Watch https://youtu.be/th4OBktqK1I at 27:00 to understand the code below
+    colums = []
+    for _ in range(cols):
+        colums = []
+        current_symbols = all_symbols[:] #[:] is a slice that copies the list
+        for _ in range (rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            colums.append(value)
+        
+        colums.append(colums)
+
+    return colums
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i == len(columns) - 1:
+                print(column[row], "|")
+            else:
+                print(column[row])
+
+
 def deposit():
     while True:
         amount = input("What would you like to depsit? $")
@@ -31,6 +65,7 @@ def deposit():
             print("Please enter a valid amount")
     return amount
 
+
 def get_number_of_lines():
     while True:
         lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")# we make a concatenation to make sure that the user can only enter a number between 1 and 3
@@ -44,6 +79,7 @@ def get_number_of_lines():
             print("Please enter a valid number of lines")
     return lines
 
+
 def get_bet():
     while True:
         bet = input("What would you like to bet ?")
@@ -56,6 +92,7 @@ def get_bet():
         else:
             print("Please enter a valid bet")
     return bet
+
 
 #We use the main function so that if we end our program, we can start it again
 def main():
@@ -71,3 +108,5 @@ def main():
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}")
 
 main()
+
+#I stopped the video at 34:00
